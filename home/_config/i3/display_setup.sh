6b54ff971f52595419e3f7ecd9d3426c72f66a50
Nyan <git@nyan.su>
2023-03-09 22:40:47 +0300

    remove display config for bluebook

--- home/_config/i3/display_setup.sh
+++ home/_config/i3/display_setup.sh
@@ -4,14 +4,8 @@ function whitebox_init() {
     xrandr --output HDMI-0 --primary --right-of DVI-D-0
 }
 
-function bluebook_init() {
-    xrandr --auto
-}
-
 case $HOSTNAME in
     whitebox)
         whitebox_init;;
-    bluebook)
-        bluebook_init;;
     *) echo "hostname not found";;
 esac
