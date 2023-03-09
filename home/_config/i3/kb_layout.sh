ceadeb036039857c32283b004bb1976b1d419773
Nyan <git@nyan.su>
2023-03-10 00:59:10 +0300

    what the ooooooooooooooooooooooo

---
+++ home/_config/i3/kb_layout.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+function whitebox_kb_setup() {
+    setxkbmap -model pc104 -layout us,ru -option grp:alt_shift_toggle
+}
+
+case $HOSTNAME in
+    whitebox)
+        whitebox_kb_setup;;
+    *) echo "hostname not found";;
+esac
--- home/_config/polybar/launch.sh
+++ home/_config/polybar/launch.sh
@@ -6,7 +6,15 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar first &
-polybar second &
+case $HOSTNAME in
+    whitebox)
+        export PRIMARY_MONITOR=HDMI-0
+        export SECONDARY_MONITOR=DVI-D-0
+        ;;
+    *) echo "default";;
+esac
+
+polybar primary &
+polybar secondary &
 
 echo "Polybar launched..."
