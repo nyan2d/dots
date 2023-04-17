66d8917c9319d4962fecea2ad3b8443d5e28e5b1
Nyan <git@nyan.su>
2023-04-17 13:40:32 +0300

    add display configs

--- home/_config/i3/display_setup.sh
+++ home/_config/i3/display_setup.sh
@@ -4,8 +4,16 @@ function whitebox_init() {
     xrandr --output HDMI-0 --primary --right-of DVI-D-0
 }
 
+function imslow_init() {
+    xrandr --output HDMI-5 --primary --right-of HDMI-4 --rotate left
+}
+
 case $HOSTNAME in
     whitebox)
         whitebox_init;;
+    imslow)
+        imslow_init;;
     *) echo "hostname not found";;
 esac
+
+echo $HOSTNAME
--- home/_config/polybar/launch.sh
+++ home/_config/polybar/launch.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# TODO: rewrite
-# ref: https://github.com/jayywolff/dotfiles/tree/master/.config/polybar
-
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
@@ -10,6 +7,10 @@ case $HOSTNAME in
     whitebox)
         export PRIMARY_MONITOR=HDMI-0
         export SECONDARY_MONITOR=DVI-D-0
+        ;;
+    imslow)
+        export PRIMARY_MONITOR=HDMI-4
+        export SECONDARY_MONITOR=HDMI-5
         ;;
     *) echo "default";;
 esac
